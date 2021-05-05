2f34e231fbbfb095c460c6db7398f7696f1be7cf
ReeSuro <surowiezs@gmail.com>
2021-05-06 10:43:24 +1200

    Create the Visual Studio 2019 Solution for the project
    The solution is titled "OverboardDetectioMonitor" with a starting project
    "DetectionCOnsole". This solution will contain all of the projects
    surrounding the monitoring software for the MOB system.

---
+++ OverboardDetectionMonitor/DetectionConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DetectionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
